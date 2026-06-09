bf162308a33166c45b445ffb5310921605c9e8b8
hp <hp@host.docker.internal>
2026-06-09 16:30:08 +0530

    First Commit

---
+++ src/main/java/gov/dolr/wdcpmksy3/Wdcpmksy3Application.java
@@ -0,0 +1,13 @@
+package gov.dolr.wdcpmksy3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wdcpmksy3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wdcpmksy3Application.class, args);
+	}
+
+}
---
+++ src/test/java/gov/dolr/wdcpmksy3/Wdcpmksy3ApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.dolr.wdcpmksy3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wdcpmksy3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
