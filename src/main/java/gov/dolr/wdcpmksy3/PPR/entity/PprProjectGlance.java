b451b60e1b2a4fae8e90d5bea76c92d2c3065698
hp <hp@host.docker.internal>
2026-08-18 11:04:02 +0530

    commit by Yogesh

--- src/main/java/gov/dolr/wdcpmksy3/PPR/entity/PprProjectGlance.java
+++ src/main/java/gov/dolr/wdcpmksy3/PPR/entity/PprProjectGlance.java
@@ -15,41 +15,127 @@ public class PprProjectGlance {
     @Column(name="ppr_project_glance_id")
     private Integer pprProjectGlanceId;
 
-    @ManyToOne
-    @JoinColumn(name="ppr_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ppr_id", nullable = false)
     private MPpr ppr;
 
-    @ManyToOne
-    @JoinColumn(name="mw_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mw_id", nullable = false)
     private MicroWatershed microWatershed;
 
-    @ManyToOne
-    @JoinColumn(name="project_type_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_type_id", nullable = false)
     private ProjectType projectType;
 
-    @ManyToOne
-    @JoinColumn(name="pia_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="pia_id", nullable = false)
     private MPiaDetails pia;
 
-    @Column(name="selection_reason")
+    @Column(name="selection_reason", length = 200)
     private String selectionReason;
 
-    @Column(name="project_area")
+    @Column(name="project_area", precision = 20, scale = 4)
     private BigDecimal projectArea;
 
-    @Column(name="proposed_area")
+    @Column(name="proposed_area", precision = 20, scale = 4)
     private BigDecimal proposedArea;
 
-    @Column(name="project_cost")
+    @Column(name="project_cost", precision = 20, scale = 4)
     private BigDecimal projectCost;
 
-    @Column(name="comments")
+    @Column(name="comments", length = 200)
     private String comments;
 
-    @Column(name="status")
+    @Column(name="status", length = 1)
     private Character status;
 
-    @OneToMany(mappedBy="projectGlance",cascade=CascadeType.ALL)
-    private List<PprVillage> villages;
+	public Integer getPprProjectGlanceId() {
+		return pprProjectGlanceId;
+	}
+
+	public void setPprProjectGlanceId(Integer pprProjectGlanceId) {
+		this.pprProjectGlanceId = pprProjectGlanceId;
+	}
+
+	public MPpr getPpr() {
+		return ppr;
+	}
+
+	public void setPpr(MPpr ppr) {
+		this.ppr = ppr;
+	}
+
+	public MicroWatershed getMicroWatershed() {
+		return microWatershed;
+	}
+
+	public void setMicroWatershed(MicroWatershed microWatershed) {
+		this.microWatershed = microWatershed;
+	}
+
+	public ProjectType getProjectType() {
+		return projectType;
+	}
+
+	public void setProjectType(ProjectType projectType) {
+		this.projectType = projectType;
+	}
+
+	public MPiaDetails getPia() {
+		return pia;
+	}
+
+	public void setPia(MPiaDetails pia) {
+		this.pia = pia;
+	}
+
+	public String getSelectionReason() {
+		return selectionReason;
+	}
+
+	public void setSelectionReason(String selectionReason) {
+		this.selectionReason = selectionReason;
+	}
+
+	public BigDecimal getProjectArea() {
+		return projectArea;
+	}
+
+	public void setProjectArea(BigDecimal projectArea) {
+		this.projectArea = projectArea;
+	}
+
+	public BigDecimal getProposedArea() {
+		return proposedArea;
+	}
+
+	public void setProposedArea(BigDecimal proposedArea) {
+		this.proposedArea = proposedArea;
+	}
+
+	public BigDecimal getProjectCost() {
+		return projectCost;
+	}
+
+	public void setProjectCost(BigDecimal projectCost) {
+		this.projectCost = projectCost;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public Character getStatus() {
+		return status;
+	}
+
+	public void setStatus(Character status) {
+		this.status = status;
+	}
+
 
 }
