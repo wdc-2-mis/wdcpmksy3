a78336e946be2951563f1c92cb82f84f83172ae1
NIC <NIC@DoLR-CGO-614-Kedar-Developer>
2026-09-15 09:14:25 +0530

    kdy

--- src/main/java/gov/dolr/wdcpmksy3/PPR/repository/PprTransactionRepository.java
+++ src/main/java/gov/dolr/wdcpmksy3/PPR/repository/PprTransactionRepository.java
@@ -25,7 +25,7 @@ public interface PprTransactionRepository extends JpaRepository<PprTransaction, 
     		+ "from PprTransaction t2  where t2.ppr.pprId = t.ppr.pprId) order by t.senton desc")
     List<PprTransaction> findLatestTransactionsByState(@Param("stCode") Integer stCode);
     
-    @Query("select t from PprTransaction t where t.ppr.pprId=:pprId and t.action='F' and t.senton=(select max(t2.senton) "
+    @Query("select t from PprTransaction t where t.ppr.pprId=:pprId and (t.action='F' or t.action='A' or t.action='R') and t.senton=(select max(t2.senton) "
     		+ "from PprTransaction t2  where t2.ppr.pprId = t.ppr.pprId) order by t.senton desc")
     List<PprTransaction> findLatestTransactionsByPprPprId(@Param("pprId") Integer pprId);
 }
